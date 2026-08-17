841cae533698be6b496f5c1cb73945f18ba5e68a
Elise <60504990+Passiflore@users.noreply.github.com>
2026-08-17 22:56:14 +0200

    feat: add HTTP request logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { pinoHttp } from "pino-http";
 
 const app = express();
 const port = 3000;
+
+app.use(pinoHttp());
 
 app.get("/healthz", (request, response) => {
 	response.sendStatus(200);
