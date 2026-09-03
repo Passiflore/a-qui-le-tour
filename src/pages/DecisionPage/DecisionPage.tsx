c43b69907c4265e0c57693ac4235d52d31bf8d89
Elise <60504990+Passiflore@users.noreply.github.com>
2026-09-03 23:02:28 +0200

    feat: display current decider name on decision page

--- src/pages/DecisionPage/DecisionPage.tsx
+++ src/pages/DecisionPage/DecisionPage.tsx
@@ -2,19 +2,36 @@ import ActionButton from "../../components/ActionButton/ActionButton";
 import NameTag from "../../components/NameTag/NameTag";
 import style from "./DecisionPage.module.css";
 import DecisionDrawer from "../../components/DecisionDrawer/DecisionDrawer";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { getGame } from "../../api";
 
 function DecisionPage() {
 	const drawerRef = useRef<HTMLDialogElement>(null);
+	const gameId = localStorage.getItem("gameId");
+	const [currentDecider, setCurrentDecider] = useState<string>("");
+	useEffect(() => {
+		if (!gameId) {
+			return;
+		}
+		getGame(gameId).then((data) => {
+			if (!data.game || !data.game.guest) {
+				return;
+			}
+			if (data.game.currentDeciderPlayerId === data.game.host.id) {
+				setCurrentDecider(data.game.host.firstName);
+			} else {
+				setCurrentDecider(data.game.guest.firstName);
+			}
+		});
+	}, [gameId]);
 
 	function handleClick() {
-		// navigate("/waiting", { viewTransition: true });
 		drawerRef.current?.showModal();
 	}
 
 	return (
 		<main className={style.decisionContainer}>
-			<NameTag firstName="test" color="white" />
+			<NameTag firstName={currentDecider} color="white" />
 			<div className={style.decisionTextContainer}>
 				<h1 className={style.decisionTitle}>C'est ton tour</h1>
 				<p className={style.decisionDescription}>
